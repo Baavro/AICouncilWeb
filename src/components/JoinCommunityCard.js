48f28f9ce1780e884de84bda649b6d30b0036da0
Sankalp Patidar <sankalppatidar@kgpian.iitkgp.ac.in>
2023-10-21 12:08:35 +0530

    forth commit

--- src/components/JoinCommunityCard.js
+++ src/components/JoinCommunityCard.js
@@ -2,8 +2,16 @@ const JoinCommunityCard = () => {
   return (
     <section className="absolute h-[2.17%] w-[calc(100%_-_264.64px)] top-[15.51%] right-[134.64px] bottom-[82.32%] left-[130px] flex flex-col items-center justify-start gap-[32px] text-left text-13xl text-gray font-sora">
     <div className="relative font-extrabold">Join 18K+ community from</div>
-    <div className="flex flex-col items-start justify-start gap-[56px] text-center text-[29.72px] text-dimgray font-frank-ruhl-libre">
-        <div className="flex flex-row items-start justify-start gap-[56px]">
+    <div className="flex flex-col items-center justify-center gap-[56px] text-center text-[29.72px] text-dimgray font-frank-ruhl-libre">
+    <div className="flex flex-row items-start justify-start gap-[56px]">
+    <div className="flex flex-row items-center justify-start gap-[6.23px]">
+           <img
+             className="relative w-[28.04px] h-[27px] object-cover"
+             alt=""
+             src="/indian-institute-of-technology-bombay-logo-1@2x.png"
+           />
+           <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Bombay</div>
+         </div>
         <div className="flex flex-row items-center justify-start gap-[6.23px] ">
            <img
              className="relative w-[28.04px] h-[27px] object-cover"
@@ -28,14 +36,7 @@ const JoinCommunityCard = () => {
            />
            <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Roorkee</div>
          </div>
-         <div className="flex flex-row items-center justify-start gap-[6.23px]">
-           <img
-             className="relative w-[28.04px] h-[27px] object-cover"
-             alt=""
-             src="/indian-institute-of-technology-guwahati-logo-1@2x.png"
-           />
-           <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Guwahati</div>
-         </div>
+        
          <div className="flex flex-row items-center justify-start gap-[6.23px]">
            <img
              className="relative w-[28.04px] h-[27px] object-cover"
@@ -46,6 +47,14 @@ const JoinCommunityCard = () => {
          </div>
         </div>
         <div className="flex flex-row flex-wrap items-start justify-start gap-[56px]">
+        <div className="flex flex-row items-center justify-start gap-[6.23px]">
+           <img
+             className="relative w-[28.04px] h-[27px] object-cover"
+             alt=""
+             src="/indian-institute-of-technology-guwahati-logo-1@2x.png"
+           />
+           <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Guwahati</div>
+         </div>
         <div className="flex flex-row items-center justify-start gap-[6.23px]">
            <img
              className="relative w-[28.04px] h-[27px] object-cover"
@@ -62,14 +71,7 @@ const JoinCommunityCard = () => {
            />
            <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Mandi</div>
          </div>
-         <div className="flex flex-row items-center justify-start gap-[6.23px]">
-           <img
-             className="relative w-[28.04px] h-[27px] object-cover"
-             alt=""
-             src="/indian-institute-of-technology-bombay-logo-1@2x.png"
-           />
-           <div className="relative font-medium w-[calc(100% - 28.04px)]">IIT Bombay</div>
-         </div>
+    
          <div className="flex flex-row items-center justify-start gap-[6.23px]">
            <img
              className="relative w-[28.04px] h-[27px] object-cover"
